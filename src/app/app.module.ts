0e0e599b5c78cda41d5994608a92d603cffac6a7
dinesh <dinesh.bharti.be16@gmail.com>
2023-11-09 04:26:46 -0800

    employee profile show work

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ListofStudentComponent } from './listof-student/listof-student.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListofStudentComponent
   ],
   imports: [
     BrowserModule,
